59c1de74178df9892f7c525dbc8dd147cfebb2bf
JoseLuis Asus <jozeluin@gamil.com>
2025-10-18 17:38:55 +0200

    Proyectos-RamdomQuoteMachine

---
+++ RamdomQuoteMachine/index.js
@@ -0,0 +1,15 @@
+const App=()=>{
+
+    const [quote,setQuotes]=React.useState([]);
+    const [randomQuote,setRandomQuote]=React.useState([]);
+
+    React.useEffect(()=>{
+
+    
+    })
+
+return(
+        <div>Hola Mundosssss</div>
+    );
+}
+ReactDOM.render(<App/>,document.getElementById('app'))
