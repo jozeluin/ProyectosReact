89563fccd3c2194ee3f7946ff4ad523db27b5228
Jose Luis Perez <jozeluin@gmail.com>
2025-10-24 21:26:01 +0200

    Ya funciona y twitea la cita, con su icono

---
+++ RamdomQuoteMachine/README.md
@@ -0,0 +1,13 @@
+# Ramdom Quote Machine 
+Primer proyecto de freebootCamp, en React.
+
+## Librerias necesitamos
+En el index.html vamos a colocar en formato html(cdn), los scripts y links que necesitamos. Hemos colocado hai el script de react y react-dom ademas del link de Boostrap. Tambien utilizaremos Babel, Babel es un transpilador de JavaScript que, en el contexto de React, se encarga de convertir código moderno (como JSX y ES6+) en código JavaScript compatible con navegadores antiguos.
+Hemos colocado un script en el body referente a babel, lo tenemos que colocar por fuerza , sino no funcionara.
+Tambien hemos colocado el link de fontawesome para utilizar las mismas fuentes que el ejemplo.
+
+## ReactDom
+Creamos un elemento el cual se renderizara en elemento con el id que indicamos del elemento ubicado en index.html
+
+## Programacion
+El nucleo lo tenemos en index.js. Crearemos dos estados, uno que coloca las cita y otro que la elige de una forma aleatoria
--- RamdomQuoteMachine/index.js
+++ RamdomQuoteMachine/index.js
@@ -14,14 +14,56 @@ const App = () => {
     fetchData();
   }, []);
 
+  const getNewQuote = () => {
+    let randomIndex = Math.floor(Math.random() * quote.length);
+    setRandomQuote(quote[randomIndex]);
+  };
+
   //https://dummyjson.com/quotes
   return (
-    <div>
-      Hola Mundosssss
-      {quote.map((quoteItem) => (
-        <div>{quoteItem.quote}</div>
-      ))}
+    <div className="container pt-5">
+      <div className="shadow-none p-5 mb-5 bg-light rounded">
+        <div className="card">
+          <div className="card-header">Citas que inspiran</div>
+          <div className="card-body">
+            {randomQuote ? (
+              <>
+                <h5 className="card-title">
+                  "{randomQuote.author || "No author"}"
+                </h5>
+                <p className="card-text">- {randomQuote.quote}</p>
+              </>
+            ) : (
+              <h2>Cargando...</h2>
+            )}
+            <div className="d-inline">
+              <button onClick={getNewQuote} className="btn btn-primary ml-3">
+                New Quote
+              </button>
+              <a
+                href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                  randomQuote.quote
+                )}`}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="btn btn-info mx-2"
+              >
+                
+                <i class="fa-brands fa-twitter"></i>
+              </a>
+              
+              <a href="https://www.tumblr.com/" className="btn btn-danger">
+                <i class="fa-brands fa-tumblr"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 ReactDOM.render(<App />, document.getElementById("app"));
+
+// {/* {quote.map((quoteItem) => (
+//         <div>{quoteItem.quote}</div>
+//       ))} */}
