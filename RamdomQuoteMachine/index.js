999640a23ad8e5979c7aaf3dba9651499ce44b98
JoseLuis Asus <jozeluin@gamil.com>
2025-10-24 18:50:13 +0200

    Agregamos fetch, tenemos acceso a la api

--- RamdomQuoteMachine/index.js
+++ RamdomQuoteMachine/index.js
@@ -1,15 +1,27 @@
-const App=()=>{
+const App = () => {
+  const [quote, setQuotes] = React.useState([]);
+  const [randomQuote, setRandomQuote] = React.useState([]);
 
-    const [quote,setQuotes]=React.useState([]);
-    const [randomQuote,setRandomQuote]=React.useState([]);
+  React.useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("https://dummyjson.com/quotes");
+      const data = await response.json();
 
-    React.useEffect(()=>{
+      setQuotes(data.quotes);
+      let randomIndex = Math.floor(Math.random() * data.quotes.length);
+      setRandomQuote(data.quotes[randomIndex]);
+    }
+    fetchData();
+  }, []);
 
-    
-    })
-
-return(
-        <div>Hola Mundosssss</div>
-    );
-}
-ReactDOM.render(<App/>,document.getElementById('app'))
+  //https://dummyjson.com/quotes
+  return (
+    <div>
+      Hola Mundosssss
+      {quote.map((quoteItem) => (
+        <div>{quoteItem.quote}</div>
+      ))}
+    </div>
+  );
+};
+ReactDOM.render(<App />, document.getElementById("app"));
