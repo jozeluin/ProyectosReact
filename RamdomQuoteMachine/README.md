fa93f75a422e507d8aad11804231bd6c2663aff1
Jose Luis Perez <jozeluin@gmail.com>
2025-10-26 23:20:17 +0100

    Acabamos de documentar proyecto

--- RamdomQuoteMachine/README.md
+++ RamdomQuoteMachine/README.md
@@ -2,12 +2,28 @@
 Primer proyecto de freebootCamp, en React.
 
 ## Librerias necesitamos
-En el index.html vamos a colocar en formato html(cdn), los scripts y links que necesitamos. Hemos colocado hai el script de react y react-dom ademas del link de Boostrap. Tambien utilizaremos Babel, Babel es un transpilador de JavaScript que, en el contexto de React, se encarga de convertir código moderno (como JSX y ES6+) en código JavaScript compatible con navegadores antiguos.
+En el index.html vamos a colocar en formato html(cdn), los scripts y links que necesitamos. Hemos colocado hay el script de react y react-dom ademas del link de Boostrap. Tambien utilizaremos Babel, Babel es un transpilador de JavaScript que, en el contexto de React, se encarga de convertir código moderno (como JSX y ES6+) en código JavaScript compatible con navegadores antiguos.
 Hemos colocado un script en el body referente a babel, lo tenemos que colocar por fuerza , sino no funcionara.
 Tambien hemos colocado el link de fontawesome para utilizar las mismas fuentes que el ejemplo.
 
 ## ReactDom
-Creamos un elemento el cual se renderizara en elemento con el id que indicamos del elemento ubicado en index.html
+Creamos un elemento el cual se renderizara un elemento con el id que indicamos del elemento ubicado en index.html
+
+```javascript
+ReactDOM.render(<App />, document.getElementById("app"));
+
+```
 
 ## Programacion
-El nucleo lo tenemos en index.js. Crearemos dos estados, uno que coloca las cita y otro que la elige de una forma aleatoria
+El nucleo lo tenemos en index.js. Crearemos dos estados, uno que coloca las cita y otro que la elige de una forma aleatoria.
+
+1. ### UseEffect
+    Se cargan las citas de una api. Se utiliza el "setQuotes" para cargar las citas en "quote".
+    Despues conseguimos la cita aleatoria con "ranmdomIndex" y "setRandomQuotes".
+2. ### Mostrar info
+    Mostramos en el return la cita y el autor, y clicando el boton llamamos a "getNewQuote" para generar otra cita aleatoria.
+3. ### Iconos Redes Sociales
+    Al pulsar el correspondiente icono, llevamos la cita a un red social. Nos hemos fijado en  internet, como hacerlo
+
+4. ### Damos formato con Boostrap
+    Mediante Boostrap damos formatos a los "div", y restantes elementos
--- RamdomQuoteMachine/index.js
+++ RamdomQuoteMachine/index.js
@@ -14,6 +14,7 @@ const App = () => {
     fetchData();
   }, []);
 
+
   const getNewQuote = () => {
     let randomIndex = Math.floor(Math.random() * quote.length);
     setRandomQuote(quote[randomIndex]);
@@ -25,34 +26,29 @@ const App = () => {
       <div className="shadow-none p-5 mb-5 bg-light rounded">
         <div className="card">
           <div className="card-header">Citas que inspiran</div>
-          <div className="card-body">
+          <div className="card-body" id="quote-box">
             {randomQuote ? (
               <>
-                <h5 className="card-title">
-                  "{randomQuote.author || "No author"}"
-                </h5>
-                <p className="card-text">- {randomQuote.quote}</p>
+                <h5 className="card-title" id="author"> "{randomQuote.author || "No author"}"</h5>
+                <p className="card-text" id="text">{randomQuote.quote}</p>
               </>
             ) : (
               <h2>Cargando...</h2>
             )}
             <div className="d-inline">
-              <button onClick={getNewQuote} className="btn btn-primary ml-3">
-                New Quote
-              </button>
-              <a
-                href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
-                  randomQuote.quote
-                )}`}
+              <button onClick={getNewQuote} className="btn btn-primary ml-3" id="new-quote">New Quote</button>
+              <a href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(randomQuote.quote)}`}
                 target="_blank"
                 rel="noopener noreferrer"
                 className="btn btn-info mx-2"
+                id="tweet-quote"
               >
-                
                 <i class="fa-brands fa-twitter"></i>
               </a>
-              
-              <a href="https://www.tumblr.com/" className="btn btn-danger">
+
+             
+              <a href={`https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=,&caption=${randomQuote.author}&content=${randomQuote.quote}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button
+randomQuote.quote)}`}className="btn btn-danger">
                 <i class="fa-brands fa-tumblr"></i>
               </a>
             </div>
@@ -62,8 +58,6 @@ const App = () => {
     </div>
   );
 };
+
 ReactDOM.render(<App />, document.getElementById("app"));
 
-// {/* {quote.map((quoteItem) => (
-//         <div>{quoteItem.quote}</div>
-//       ))} */}
