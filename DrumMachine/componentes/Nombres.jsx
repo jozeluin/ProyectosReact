8472beb3c5898ded978cf234cb141cdff222eceb
JoseLuis Asus <jozeluin@gamil.com>
2025-11-16 20:34:13 +0100

    Haciendo Pruebas

---
+++ DrumMachine/componentes/Nombres.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Nombres = ({nombre}) => {
+  return (
+    <div>Mi mombre es nombre: {nombre}</div>
+  )
+}
+
+export default Nombres
--- DrumMachine/vite-project/src/App.jsx
+++ DrumMachine/vite-project/src/App.jsx
@@ -1,10 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import Nombres from "../../componentes/Nombres";
 
 function App() {
   return (
     <>
       <button className="btn btn-primary">Este es mi boton</button>
+      <Nombres nombre="Juan" />
+
     </>
   );
 }
