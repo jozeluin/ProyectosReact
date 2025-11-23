5799df98f1b4de853a8f657b97274dd382a27ae1
Jose Luis Perez <jozeluin@gmail.com>
2025-11-24 00:46:22 +0100

    Terminado sin estilo

--- DrumMachine/componentes/DumControle.jsx
+++ DrumMachine/componentes/DumControle.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const DumControle = ({name,ChangeSoundsGroup}) => {
+const DumControle = ({stop,name, power, volume,handVolumeChange,ChangeSoundsGroup}) => {
   return (
     <div className='controle'>
+      <button onClick={stop}>Turn the power{power ? "OFF" : "ON"}</button>
+      <h2>Volume: %{Math.round(volume * 100)}</h2>
+      <input
+      max="1"
+      min="0"
+      step="0.01"
+      type="range"
+      value={volume}
+      onChange={handVolumeChange}
+      />
+
       <h2 id="display">{name}</h2>
         <button onClick={ChangeSoundsGroup}>Change Sounds Group</button>
     </div>
