1af6f6932fdfd79b9c9dc3c8f88d8cdb24359e8f
JoseLuis Asus <jozeluin@gamil.com>
2025-12-19 13:42:36 +0100

    El setState al final

--- Calculator/src/components/Calcu.jsx
+++ Calculator/src/components/Calcu.jsx
@@ -34,11 +34,13 @@ const Calcu = () => {
 
   const evaluar = () => {
     let formula = state.formula;
-    while (finalizaOperador.test(formula)) formula = formula.slice(0 - 1);
+    while (finalizaOperador.test(formula)){
+      formula = formula.slice(0 - 1);
+    } 
     let resultado = eval(formula);
     setState({
       ...state,
-      formula: formula,
+      formula: formula + "=" + resultado,
       valorActual: resultado,
       evaluado: true,
     });
@@ -49,54 +51,66 @@ const Calcu = () => {
    */
   const operador = (e) => {
     const val = e.target.value;
-    const { formula: f, valorPrevio: vp, evaluado: ev } = state;
-    setState({ ...state, evaluado: false, valorActual: val });
+    let { formula: f, valorPrevio: vp, evaluado: ev } = state;
+
+    //Atencion setState({ ...state, evaluado: false, valorActual: val });
     //Si le he dado ya al igual
-    ev ? setState({...state,formula: vp + val })
-          //formula=valorPrevio+valorActual
-          
-          //Si no le he dado al igual entonces, preguntamos si la formula finaliza con un operador matematico
-          //Si es asi volvemos a preguntar si es un digito seguido de signo matematico seguido de menos
-          //Despues preguntamos si el signo menos No es igual a lo que hemos introducido, si es asi la fomula=ValorPrevio+valorIntroducido
-       
-      : finalizaOperador.test(f) ? finOperNeg.test(f) ? "-" !== val && setState({...state,formula: vp + val})
-           
-      //Si anteriormente habia un signo "-",entonces testeamos si la formula con lo que introducimos termina en "-", si es asi es solo la formula, y despues se
-            //agrega ese signo.
+    ev? (f = vp + val): //formula=valorPrevio+valorActual
 
-        : setState({...state,formula: (finOperNeg.test(f + val) ? f : vp) + val })
-            //Opcion por defecto.(ev=false) si no hemos dado al "=" y es un numero seguido de un operador.
-      : setState({...state,
-          valorPrevio: f,
-          formula: f + val,
-          valorActual:val
-        });
-  };
+      //Si no le he dado al igual entonces, preguntamos si la formula finaliza con un operador matematico
+      //Si es asi volvemos a preguntar si es un digito seguido de signo matematico seguido de menos
+      //Despues preguntamos si el signo menos No es igual a lo que hemos introducido, si es asi la fomula=ValorPrevio+valorIntroducido
 
-  const inNumero = (e) => {
-    if(!state.valorActual.includes("Limite")){
-      const puls = e.target.value;
-    /**
-     * valorActual: ultima pulsacion
-     */
-    const { formula: f, valorActual: v, evaluado: ev } = state;
-    setState({...state,evaluar:false}) , 
-    v.length>15 ? MaxLimitPul() : ev ? setState({
-      ...state,
-      valorActual:puls,
-      formula:"0" !== puls ? puls:""
-    }):
+      finalizaOperador.test(f)
+      ? finOperNeg.test(f)
+        ? "-" !== val && (f = vp + val)
+        : //Si anteriormente habia un signo "-",entonces testeamos si la formula con lo que introducimos termina en "-", si es asi es solo la formula, y despues se
+          //agrega ese signo.
 
+          (f = (finOperNeg.test(f + val) ? f : vp) + val)
+      : //Opcion por defecto.(ev=false) si no hemos dado al "=" y es un numero seguido de un operador.
+        ((vp = f), (f = f + val));
 
     setState({
       ...state,
-      //si lo que habia en valorActual es = 0 o si es un operador val va a valorActual(e.target.value) si no , se añade a lo que habia
-      valorActual: "0" === v || isOperator.test(v) ? puls : v + puls,
-      formula:"0" === v && "0" === puls ? "" === f ? puls: 
-      f: /([^.0-9]0|^0)$/.test(f)? f.slice(0, -1) + puls: f + puls,
+      evaluado: false,
+      valorActual: val,
+      formula: f,
     });
+  };
+
+
+
+  const inNumero = (e) => {
+    if (!state.valorActual.includes("Limite")) {
+      const puls = e.target.value;
+      /**
+       * valorActual: ultima pulsacion
+       */
+      const { formula: f, valorActual: v, evaluado: ev } = state;
+      setState({ ...state, evaluar: false }),
+        v.length > 15
+          ? MaxLimitPul()
+          : ev
+          ? setState({
+              ...state,
+              valorActual: puls,
+              formula: "0" !== puls ? puls : "",
+            })
+          : setState({
+              ...state,
+              //si lo que habia en valorActual es = 0 o si es un operador val va a valorActual(e.target.value) si no , se añade a lo que habia
+              valorActual: "0" === v || isOperator.test(v) ? puls : v + puls,
+              formula:
+                "0" === v && "0" === puls
+                  ? "" === f
+                    ? puls
+                    : f
+                  : /([^.0-9]0|^0)$/.test(f)
+                  ? f.slice(0, -1) + puls
+                  : f + puls,
+            });
     }
-    
   };
 
   const MaxLimitPul = () => {
@@ -114,7 +128,7 @@ const Calcu = () => {
       formula: "",
       signoActual: "+",
       evaluado: false,
-      ultimaTecla:""
+      ultimaTecla: "",
     });
   };
 
