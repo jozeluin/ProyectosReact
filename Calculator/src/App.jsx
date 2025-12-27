498f74e5330b5e4922f06ac7c4feb920675b34a1
Jose Luis Perez <jozeluin@gmail.com>
2025-12-27 13:19:13 +0100

    Finalizado

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,11 +1,9 @@
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import Tecla from "./components/tecla";
-import Teclado from "./components/Teclado";
-import KeyBoarad from "./components/keyBoarad";
+
 import Calcu from "./components/Calcu";
-import Prueba from "./components/prueba";
+
 function App() {
   return (
     <>
--- Calculator/src/components/Calcu.jsx
+++ Calculator/src/components/Calcu.jsx
@@ -1,5 +1,4 @@
-import "./Teclado.css";
-import Tecla from "./tecla";
+
 import "../App.css";
 import { useState } from "react";
 
